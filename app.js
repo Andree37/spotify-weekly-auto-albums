aec2194a2ec83ea01e1b4c4c11221528761b5bd8
Andree37 <andresmribeiro@gmail.com>
2023-03-24 01:53:40 +0000

    removed unnecessary scopes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/login', function (req, res) {
     res.cookie(stateKey, state);
 
     // your application requests authorization
-    const scope = 'user-read-private user-read-email user-library-modify';
+    const scope = 'user-library-modify';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
